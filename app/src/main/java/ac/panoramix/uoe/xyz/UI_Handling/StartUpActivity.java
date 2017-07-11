f216b71765f4456bc3490caaeed8ca2472d2df80
Chris Campbell <campbellC@users.noreply.github.com>
2017-07-11 15:26:03 +0100

    Switched from shared preferences to a flat file for storing the account of the user

--- app/src/main/java/ac/panoramix/uoe/xyz/UI_Handling/StartUpActivity.java
+++ app/src/main/java/ac/panoramix/uoe/xyz/UI_Handling/StartUpActivity.java
@@ -5,11 +5,19 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 import ac.panoramix.uoe.xyz.Accounts.Account;
 import ac.panoramix.uoe.xyz.R;
+import ac.panoramix.uoe.xyz.Utility;
 import ac.panoramix.uoe.xyz.XYZApplication;
 import ac.panoramix.uoe.xyz.XYZConstants;
 
@@ -20,22 +28,38 @@ public class StartUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up);
 
-        SharedPreferences prefs = getApplicationContext().getSharedPreferences(XYZConstants.SHARED_PREFS_FILE, MODE_PRIVATE);
-        if(prefs.contains(XYZConstants.ACCOUNT_SHARED_PREF)) {
-            Gson gson = new Gson();
-            String json = prefs.getString(XYZConstants.ACCOUNT_SHARED_PREF, "");
-            Account alice = gson.fromJson(json, Account.class);
-            Log.d("StartUpAct", "Previous account found with name :" + alice.getUsername() );
-            XYZApplication.setAccount(alice);
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
+        File f = getBaseContext().getFileStreamPath(XYZConstants.ACCOUNT_STORAGE_FILE);
+        if(f.exists()) {
+            try {
+                FileInputStream fis = openFileInput(XYZConstants.ACCOUNT_STORAGE_FILE);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+                Account alice = (Account) ois.readObject();
+                XYZApplication.setAccount(alice);
+                ois.close();
+                fis.close();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                return;
+            } catch (FileNotFoundException e){
+                Log.d("StartUpAct", "Account File can't open", e);
+            } catch (IOException e){
+                Log.d("StartUpAct", "Account File can't be read", e);
+            } catch (ClassNotFoundException e) {
+                Log.d("StartUpAct", "Account File corrupt", e);
+            }
+            Toast.makeText(this, "Account storage damaged, you must make a new account.", Toast.LENGTH_SHORT).show();
+            launchUserRegistration();
         } else {
-            Intent intent = new Intent(getApplicationContext(), UserRegistrationActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
+            launchUserRegistration();
         }
 
 
+    }
+
+    private void launchUserRegistration() {
+        Intent intent = new Intent(getApplicationContext(), UserRegistrationActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
