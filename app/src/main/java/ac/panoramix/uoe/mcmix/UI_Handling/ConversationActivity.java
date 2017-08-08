d6f21bb661d33ea3c88b375389ece89f452bc595
Chris Campbell <campbellC@users.noreply.github.com>
2017-08-08 12:15:07 +0100

    Converted ConversationActivity to a LinearLayout. Now listview does not overlap input. Keyboard resizing works:

--- app/src/main/java/ac/panoramix/uoe/mcmix/UI_Handling/ConversationActivity.java
+++ app/src/main/java/ac/panoramix/uoe/mcmix/UI_Handling/ConversationActivity.java
@@ -7,7 +7,6 @@ import android.content.IntentFilter;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
